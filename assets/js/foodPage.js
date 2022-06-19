1319251946dd50a383eec63b2b2bce831f18b374
rhenea <rheneadianne@gmail.com>
2022-06-19 02:45:32 -0400

    added dropdown selectors, alerts if combo doesnt work, and added initial bulma test css

--- assets/js/foodPage.js
+++ assets/js/foodPage.js
@@ -3,22 +3,19 @@ const getLabelAPI = "?apiKey=84d33c0efd234661bf41e9848364e177"
 console.log(currentMenuStored)
 
 const singlePageTitle = document.querySelector(".singlePageTitle")
-
-const foodDesc = document.querySelector(".foodDesc")
 const foodSteps = document.querySelector(".foodSteps ")
 const foodNutrition = document.querySelector(".foodNutrition")
 
 singlePageTitle.innerHTML = currentMenuStored[0].recipes[0].title
-foodDesc.innerHTML = currentMenuStored[0].recipes[0].summary
 foodSteps.innerHTML = currentMenuStored[0].recipes[0].instructions
 
-fetch(`https://api.spoonacular.com/recipes/${currentMenuStored[0].recipes[0].id}/nutritionLabel.png`, {
+fetch(`https://api.spoonacular.com/recipes/${currentMenuStored[0].recipes[0].id}/nutritionLabel.png${getLabelAPI}`, {
     method: "GET",
     headers: { "Content-type": "image/png" }
 })
     .then((response) => response)
     .then((data) => {
-        console.log(data)
+        console.log(data.url)
         foodNutrition.src = data.url
 });
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,43 @@
 const foodAPIkey = "&apiKey=84d33c0efd234661bf41e9848364e177"
 const foodTitle = document.querySelector(".foodTitle")
 const foodImg = document.querySelector(".foodImg")
-let foodAPI = "https://api.spoonacular.com/recipes/random?tags=" 
+let spoonParams = {
+    recipeType: "main%20course",
+    diet: "",
+    intolerances: ""
+}
 
-$(".killme").click(function() {
+let foodAPI = `https://api.spoonacular.com/recipes/random?tags=`
+
+const changeMealType = recipeType => {
+    spoonParams.recipeType = recipeType
+    foodAPI = `https://api.spoonacular.com/recipes/random?tags=${spoonParams.recipeType},${spoonParams.diet},${spoonParams.intolerances}`
+}
+
+const changeDiet = diet => {
+    spoonParams.diet = diet
+    foodAPI = `https://api.spoonacular.com/recipes/random?tags=${spoonParams.recipeType},${spoonParams.diet},${spoonParams.intolerances}`
+}
+
+const changeIntolerance = intolerances => {
+    spoonParams.intolerances = intolerances
+    foodAPI = `https://api.spoonacular.com/recipes/random?tags=${spoonParams.recipeType},${spoonParams.diet},${spoonParams.intolerances}`
+}
+
+$(".killme").click(function () {
     fetch(foodAPI + foodAPIkey, {
         method: "GET",
         headers: { "Content-type": "application/json" }
     })
         .then((response) => response.json())
         .then((data) => {
-        let currentRandomRecipe = [data]
-        randomFood(data)
-        $(".foodTitle").click(function(){
-            saveForMoreDetails(currentRandomRecipe)
+            let currentRandomRecipe = [data]
+            randomFood(data)
+            $(".moreInfobtn").click(function () {
+                saveForMoreDetails(currentRandomRecipe)
+            })
         })
-    });
+        .catch(() => document.getElementById("demo").innerHTML = "Invalid Combo! Please try again")
 })
 
 const currentMenuStored = JSON.parse(localStorage.getItem("currentMenu"))
@@ -23,7 +45,7 @@ const currentMenuStored = JSON.parse(localStorage.getItem("currentMenu"))
 const randomFood = data => {
     foodImg.src = data.recipes[0].image
     foodTitle.innerHTML = data.recipes[0].title
-    console.log(data)
+
 }
 
 const saveForMoreDetails = currentRandomRecipe => {
