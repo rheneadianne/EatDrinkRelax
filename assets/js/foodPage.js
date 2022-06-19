197e3decb65379fcdf56c9be302cc6b9bb82bf84
rheneadianne <rheneadianne@gmail.com>
2022-06-18 20:05:06 -0400

    added desc and instructions to single page

--- assets/js/foodPage.js
+++ assets/js/foodPage.js
@@ -1,13 +1,26 @@
 const currentMenuStored = JSON.parse(localStorage.getItem("currentMenu"))
-
+const getLabelAPI = "?apiKey=84d33c0efd234661bf41e9848364e177"
 console.log(currentMenuStored)
 
 const singlePageTitle = document.querySelector(".singlePageTitle")
 
 const foodDesc = document.querySelector(".foodDesc")
+const foodSteps = document.querySelector(".foodSteps ")
+const foodNutrition = document.querySelector(".foodNutrition")
 
 singlePageTitle.innerHTML = currentMenuStored[0].recipes[0].title
 foodDesc.innerHTML = currentMenuStored[0].recipes[0].summary
+foodSteps.innerHTML = currentMenuStored[0].recipes[0].instructions
+
+fetch(`https://api.spoonacular.com/recipes/${currentMenuStored[0].recipes[0].id}/nutritionLabel.png`, {
+    method: "GET",
+    headers: { "Content-type": "image/png" }
+})
+    .then((response) => response)
+    .then((data) => {
+        console.log(data)
+        foodNutrition.src = data.url
+});
 
 $(".close").click(function(){
     window.close()
